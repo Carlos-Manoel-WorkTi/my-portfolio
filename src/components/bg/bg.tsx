bc8163ee9774fa44db615b66cfd3a15edc231051
Carlos-Manoel-WorkTi <wwwmanoelcruz924@gmail.com>
2024-08-30 01:21:54 -0300

    v5

--- src/components/bg/bg.tsx
+++ src/components/bg/bg.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import "./bg.css"
+'use client'
+import React, { useEffect, useState } from 'react';
+import "./bg.css";
+
 export default function Bg() {
+  const [offsetY, setOffsetY] = useState(0);
+ 
+  const handleScroll = () => {
+    setOffsetY(window.scrollY * 0.5); // Ajuste o fator de velocidade conforme necessário
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div id='container-bg'>
-        <div id="bg"></div>
+      <div id="bg" style={{ transform: `translateY(${offsetY}px)` }}></div>
+      <span id='star1'></span>
+      <span id='star2'></span>
+      <span id='star3'></span>
+      <span id='star4'></span>
     </div>
-  )
+  );
 }
