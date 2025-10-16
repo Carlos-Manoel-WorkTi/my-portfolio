580ec8787c96a915a04f721ee2cfec5ceda432bc
carlos <wwwmanoelcruz924@gmail.com>
2025-10-16 15:21:11 -0300

    add cors

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,6 +5,16 @@ import { ProjectItem } from "@/types/types";
 
 export async function POST(req: Request, res: NextApiResponse) {
 
+  const headers = {
+    "Access-Control-Allow-Origin": "https://portfolio-carlos-five.vercel.app", // para produção, coloque só seu domínio
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { headers, status: 200 });
+  }
+
   const title = await req.json();
   // Procurando o projeto pelo título
   const project:ProjectItem | null = await getInfByName(title.title);
