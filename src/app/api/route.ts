daffd6c23a667da59caf2444e40b6124308ff036
carlos <wwwmanoelcruz924@gmail.com>
2025-10-16 15:25:16 -0300

    cors

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,32 +3,30 @@ import { NextResponse } from "next/server";
 import { getInfAll, getInfByName } from "./firebaseStorage";
 import { ProjectItem } from "@/types/types";
 
-export async function POST(req: Request, res: NextApiResponse) {
+const headers = {
+  "Access-Control-Allow-Origin": "https://portfolio-carlos-five.vercel.app", // para produção, coloque só seu domínio
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
 
-  const headers = {
-    "Access-Control-Allow-Origin": "https://portfolio-carlos-five.vercel.app", // para produção, coloque só seu domínio
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-  };
+export async function OPTIONS() {
+  return new Response(null, { headers });
+}
 
-  if (req.method === "OPTIONS") {
-    return new NextResponse(null, { headers, status: 200 });
-  }
-
+export async function POST(req: Request) {
   const title = await req.json();
-  // Procurando o projeto pelo título
-  const project:ProjectItem | null = await getInfByName(title.title);
+  const project: ProjectItem | null = await getInfByName(title.title);
 
   if (project) {
-    return NextResponse.json(project ); // Retornando o projeto encontrado
+    return NextResponse.json(project, { headers });
   } else {
     return NextResponse.json(
       { message: "Projeto não encontrado" },
-      { status: 404 }
-    ); // Retornando erro 404 se não encontrado
+      { status: 404, headers }
+    );
   }
 }
 
 export async function GET() {
-  return NextResponse.json(await getInfAll());
+  return NextResponse.json(await getInfAll(), { headers });
 }
